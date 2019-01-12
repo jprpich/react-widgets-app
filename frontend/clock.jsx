d8b9f28943021629e2420d3b53146bc40104b550
Joshua <joshuaprpich@gmail.com>
2019-01-12 17:10:23 -0500

    initial import

---
+++ frontend/clock.jsx
@@ -0,0 +1,45 @@
+import React from 'react' 
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {time: new Date()} 
+    this.tick = this.tick.bind(this)
+  }
+
+  tick(){
+    this.setState({time: new Date()})
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  componentWillMount(){
+    clearInterval(this.interval) 
+  }
+
+  render(){
+    const seconds = this.state.time.getSeconds()
+    const minutes = this.state.time.getMinutes()
+    const hours = this.state.time.getHours()
+    const date = this.state.time.toDateString()
+    return (
+      <div>
+        <h1 class="widget-title">Clock</h1>
+        <div class="clock-div">
+          <div>
+            <h1>Time:</h1>
+            <h1>Date:</h1>
+          </div>
+          <div>
+            <h1>{hours}:{minutes}:{seconds}</h1>
+            <h1>{date}</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Clock
---
+++ root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clock from './frontend/clock'
+
+
+const Root = () => {
+  return (
+    <div>
+      <Clock />
+    </div>
+  )
+};
+
+export default Root;
